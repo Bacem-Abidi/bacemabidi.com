5fc2d6ec0e167e05d73b0bbf986a277a3d86272c
Bacem Abidi <abidi.bacem.ab.25@gmail.com>
2025-03-04 21:31:58 +0100

    Some file organization

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -127,14 +127,7 @@
                 </div>
                 <div
                     class="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10">
-                    <svg class="size-10 md:max-lg:size-8 text-text transition-all duration-300 ease-in-out group-hover:scale-75"
-                        viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none">
-                        <g stroke="currentColor" stroke-linecap="round" stroke-width="2">
-                            <path stroke-linejoin="round" d="M7 8l-4 4 4 4"></path>
-                            <path d="M10.5 18l3-12"></path>
-                            <path stroke-linejoin="round" d="M17 8l4 4-4 4"></path>
-                        </g>
-                    </svg>
+                    @include('partials.icons.icon-set', ['icon' => 'code'])
                     <h3 class="text-lg font-semibold text-text">Featured Project</h3>
                     <p class="max-w-sm text-grayBright text-sm">DroidTale is my project to natively recreate
                         'Undertale' for android devices
@@ -186,13 +179,7 @@
 
                 <div
                     class="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10">
-                    <svg class="size-10 md:max-lg:size-8 text-text transition-all duration-300 ease-in-out group-hover:scale-75"
-                        viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M9 17H15M9 13H15M9 9H10M13 3H8.2C7.0799 3 6.51984 3 6.09202 3.21799C5.71569 3.40973 5.40973 3.71569 5.21799 4.09202C5 4.51984 5 5.0799 5 6.2V17.8C5 18.9201 5 19.4802 5.21799 19.908C5.40973 20.2843 5.71569 20.5903 6.09202 20.782C6.51984 21 7.0799 21 8.2 21H15.8C16.9201 21 17.4802 21 17.908 20.782C18.2843 20.5903 18.5903 20.2843 18.782 19.908C19 19.4802 19 18.9201 19 17.8V9M13 3L19 9M13 3V7.4C13 7.96005 13 8.24008 13.109 8.45399C13.2049 8.64215 13.3578 8.79513 13.546 8.89101C13.7599 9 14.0399 9 14.6 9H19"
-                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        </path>
-                    </svg>
+                    @include('partials.icons.icon-set', ['icon' => 'blog'])
                     <h3 class="text-lg font-semibold text-text">Blog</h3>
                     <p class="max-w-sm text-grayBright text-sm">Stories from my journey as a developer and a 3D artist.
                     </p>
@@ -226,12 +213,7 @@
                 </div>
                 <div
                     class="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10">
-                    <svg class="size-10 md:max-lg:size-8 text-text transition-all duration-300 ease-in-out group-hover:scale-75"
-                        viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M21 13V17C21 18.1046 20.1046 19 19 19H9M5 19C3.89543 19 3 18.1046 3 17V9C3 7.89543 3.89543 7 5 7H7.5C8.05228 7 8.5 6.55228 8.5 6C8.5 5.44772 8.94772 5 9.5 5H14.5C15.0523 5 15.5 5.44772 15.5 6C15.5 6.55228 15.9477 7 16.5 7H19C20.1046 7 21 7.89543 21 9M9 13C9 14.6569 10.3431 16 12 16C13.6569 16 15 14.6569 15 13C15 11.3431 13.6569 10 12 10"
-                            stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path>
-                    </svg>
+                    @include('partials.icons.icon-set', ['icon' => 'camera'])
                     <h3 class="text-lg font-semibold text-text">Photography</h3>
                     <p class="max-w-sm text-grayBright text-sm">A hobby that lets me see the world differently.
                     </p>
@@ -264,25 +246,7 @@
                 </div>
                 <div
                     class="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10">
-                    <svg viewBox="0 0 24 24" fill="none"
-                        class=" size-10 md:max-lg:size-8 text-text transition-all duration-300 ease-in-out group-hover:scale-75"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M8 10.5C8 11.3284 7.32843 12 6.5 12C5.67157 12 5 11.3284 5 10.5C5 9.67157 5.67157 9 6.5 9C7.32843 9 8 9.67157 8 10.5Z"
-                            fill="currentColor"></path>
-                        <path
-                            d="M10.5 8C11.3284 8 12 7.32843 12 6.5C12 5.67157 11.3284 5 10.5 5C9.67157 5 9 5.67157 9 6.5C9 7.32843 9.67157 8 10.5 8Z"
-                            fill="currentColor"></path>
-                        <path
-                            d="M17 6.5C17 7.32843 16.3284 8 15.5 8C14.6716 8 14 7.32843 14 6.5C14 5.67157 14.6716 5 15.5 5C16.3284 5 17 5.67157 17 6.5Z"
-                            fill="currentColor"></path>
-                        <path
-                            d="M7.5 17C8.32843 17 9 16.3284 9 15.5C9 14.6716 8.32843 14 7.5 14C6.67157 14 6 14.6716 6 15.5C6 16.3284 6.67157 17 7.5 17Z"
-                            fill="currentColor"></path>
-                        <path fill-rule="evenodd" clip-rule="evenodd"
-                            d="M1 12C1 5.92487 5.92487 1 12 1C17.9712 1 23 5.34921 23 11V11.0146C23 11.543 23.0001 12.4458 22.6825 13.4987C21.8502 16.2575 18.8203 16.9964 16.4948 16.4024C16.011 16.2788 15.5243 16.145 15.0568 16.0107C14.2512 15.7791 13.5177 16.4897 13.6661 17.2315L13.9837 18.8197L14.0983 19.5068C14.3953 21.289 13.0019 23.1015 11.0165 22.8498C7.65019 22.423 5.11981 21.1007 3.43595 19.1329C1.75722 17.171 1 14.6613 1 12ZM12 3C7.02944 3 3 7.02944 3 12C3 14.2854 3.64673 16.303 4.95555 17.8326C6.25924 19.3561 8.3 20.4894 11.2681 20.8657C11.7347 20.9249 12.2348 20.4915 12.1255 19.8356L12.0163 19.1803L11.7049 17.6237C11.2467 15.3325 13.4423 13.4657 15.6093 14.0885C16.0619 14.2186 16.529 14.3469 16.9897 14.4646C18.7757 14.9208 20.3744 14.2249 20.7677 12.921C20.997 12.161 21 11.5059 21 11C21 6.65079 17.0745 3 12 3Z"
-                            fill="currentColor"></path>
-                    </svg>
+                    @include('partials.icons.icon-set', ['icon' => 'palette'])
                     <h3 class="text-lg font-semibold text-text">More Than Just Code</h3>
                     <p class="max-w-sm text-grayBright text-sm">A developer, a 3D artist, and a creator bringing ideas to
                         life in
@@ -308,7 +272,7 @@
                             @for ($i = 0; $i < 4; $i++)
                                 <div
                                     class="flex shrink-0 justify-around [gap:var(--gap)] motion-reduce:animate-none animate-marquee flex-row">
-                                    @include('partials.svg-icons')
+                                    @include('partials.icons.tech-stack-icons')
                                 </div>
                             @endfor
                         </div>
@@ -331,11 +295,7 @@
                     </div>
                 </div>
                 <div class="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" stroke="#000000"
-                        stroke-width="1" class=" size-10 md:max-lg:size-8 text-text">
-                        <path
-                            d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" />
-                    </svg>
+                    @include('partials.icons.icon-set', ['icon' => 'location'])
                     <h3 class="text-lg font-semibold text-text">Location</h3>
                     <p class="max-w-lg text-grayBright text-sm">Currently Based In Tunisia</p>
                 </div>
@@ -343,5 +303,12 @@
         </div>
 
     </section>
+
+
+
+
+
+
+    
 
 @endsection
